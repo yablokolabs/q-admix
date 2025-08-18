b3d93e126f1ae861c6ae4eeec3c57b76e4f1a073
CEO <ceo@map2map.com>
2025-08-18 09:39:41 +0000

    Add cloudflare analytics

--- q-admix/pages/_document.js
+++ q-admix/pages/_document.js
@@ -3,7 +3,13 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang='en'>
-      <Head />
+      <Head>
+        <script 
+          defer 
+          src='https://static.cloudflareinsights.com/beacon.min.js' 
+          data-cf-beacon='{"token": "ac63bbdd99d645309172c615bf8ec446"}'
+        />
+      </Head>
       <body className='bg-gray-900'>
         <Main />
         <NextScript />
